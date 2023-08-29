f6a856fa5175937e2601ad44b64df0103ba31c39
Chengzhong <chengzhongpr@gmail.com>
2023-08-29 15:05:34 -0700

    add weapon and basic dps calculation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import {
+  getDamageRange,
+  getDpsValue,
+  getWeaponDamage,
+} from "./utilities/calculations";
 
 function App() {
+  const [weaponLevel, setWeaponLevel] = useState(1);
+  const weaponMinLevel = 1,
+    weaponMaxLevel = 7;
+  const [isWeaponLevelDownDisabled, setWeaponLevelDownDisabled] =
+    useState(true);
+  const [isWeaponLevelUpDisabled, setWeaponLevelUpDisabled] = useState(false);
+  const onWeaponLevelUp = () => {
+    setWeaponLevel(weaponLevel + 1);
+    setWeaponLevelDownDisabled(weaponLevel + 1 === weaponMinLevel);
+    setWeaponLevelUpDisabled(weaponLevel + 1 === weaponMaxLevel);
+  };
+  const onWeaponLevelDown = () => {
+    setWeaponLevel(weaponLevel - 1);
+    setWeaponLevelUpDisabled(weaponLevel - 1 === weaponMaxLevel);
+    setWeaponLevelDownDisabled(weaponLevel - 1 === weaponMinLevel);
+  };
+  const hp = 50,
+    baseAtk = 10,
+    externalAtk = 100,
+    speed = 100,
+    anvilCount = 0,
+    weaponDmg = getWeaponDamage("weapon", weaponLevel),
+    growthBonus = 2,
+    critRate = 10,
+    critMultiplier = 150,
+    haste = 10,
+    baseHitPerSecond = 4;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        Character Stats
+        <table className="stats-table">
+          <thead>
+            <tr>
+              <td>Stats</td>
+              <td>Value</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>HP</td>
+              <td>{hp}</td>
+            </tr>
+            <tr>
+              <td>Attack</td>
+              <td>{externalAtk}%</td>
+            </tr>
+            <tr>
+              <td>Speed</td>
+              <td>{speed}%</td>
+            </tr>
+            <tr>
+              <td>Haste</td>
+              <td>{haste}%</td>
+            </tr>
+            <tr>
+              <td>Crit Rate</td>
+              <td>{critRate}%</td>
+            </tr>
+            <tr>
+              <td>Crit damage</td>
+              <td>{critMultiplier}%</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        Weapon level{" "}
+        <button
+          disabled={isWeaponLevelDownDisabled}
+          onClick={onWeaponLevelDown}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <span> {weaponLevel} </span>
+        <button disabled={isWeaponLevelUpDisabled} onClick={onWeaponLevelUp}>
+          +
+        </button>
+      </div>
+      <div>
+        DPS:{" "}
+        {getDpsValue(
+          getDamageRange(
+            baseAtk,
+            externalAtk,
+            anvilCount,
+            weaponDmg,
+            growthBonus,
+            critRate,
+            critMultiplier
+          ).average,
+          haste,
+          baseHitPerSecond
+        ) || 100}
+      </div>
     </div>
   );
 }
---
+++ src/utilities/calculations.js
@@ -0,0 +1,58 @@
+// =(10*[0.7~1.3]*(CharacterBAseAtk+ExternalAtkBonus)*(0.2*AnvilCount+WpnDmg+2%*GrowthBonus))*(1+IF((TotalCritRate)>100%,100%,IF((TotalCritRate)<0%,0%,TotalCritRate))*CritMultiplier)
+
+export function getDamageRange(
+  baseAtk,
+  externalAtk,
+  anvilCount,
+  weaponDmg,
+  growthBonus,
+  critRate,
+  critMultiplier
+) {
+  const actualCritRate =
+    (critRate > 100 ? 100 : critRate < 0 ? 0 : critRate) * 0.01;
+  const lower =
+    10 *
+    0.7 *
+    (baseAtk + externalAtk * 0.01) *
+    (0.2 * anvilCount + weaponDmg + 0.02 * growthBonus) *
+    ((1 + actualCritRate) * critMultiplier * 0.01);
+  const upper =
+    10 *
+    1.3 *
+    (baseAtk + externalAtk) *
+    (0.2 * anvilCount + weaponDmg + 0.02 * growthBonus) *
+    ((1 + actualCritRate) * critMultiplier * 0.01);
+  const damageOutput = {
+    lower: lower,
+    upper: upper,
+    average: 0.5 * (lower + upper),
+  };
+  return damageOutput;
+}
+
+export function getDpsValue(averageDamageOutput, haste, baseHitPerSecond) {
+  const dps = averageDamageOutput * baseHitPerSecond * (1 + haste * 0.01);
+  return dps.toFixed(0);
+}
+
+export function getWeaponDamage(name, level) {
+  if (name === "weapon") {
+    switch (level) {
+      case 1:
+        return 1.2;
+      case 2:
+      case 3:
+        return 1.4;
+      case 4:
+      case 5:
+        return 1.6;
+      case 6:
+        return 1.8;
+      case 7:
+        return 2;
+      default:
+        return 2;
+    }
+  }
+}
